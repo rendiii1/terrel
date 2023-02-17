d02bc2f34c10ad79d0f9880c0b444edbede126e0
rendiii1 <112694449+rendiii1@users.noreply.github.com>
2023-02-17 18:02:00 +0300

    1

---
+++ src/main/java/app/Task.java
@@ -0,0 +1,115 @@
+package app;
+
+import io.github.humbleui.jwm.MouseButton;
+import io.github.humbleui.skija.Canvas;
+import io.github.humbleui.skija.Paint;
+import io.github.humbleui.skija.Rect;
+import misc.CoordinateSystem2d;
+import misc.CoordinateSystem2i;
+import misc.Vector2d;
+import misc.Vector2i;
+import panels.PanelLog;
+
+import java.util.ArrayList;
+
+/**
+ * Класс задачи
+ */
+public class Task {
+    /**
+     * Текст задачи
+     */
+    public static final String TASK_TEXT = """
+            ПОСТАНОВКА ЗАДАЧИ:
+            Заданы два множества точек в вещественном
+            пространстве. Требуется построить пересечение
+            и разность этих множеств""";
+
+    /**
+     * Вещественная система координат задачи
+     */
+    private final CoordinateSystem2d ownCS;
+    /**
+     * Список точек
+     */
+    private final ArrayList<Point> points;
+    /**
+     * Размер точки
+     */
+    private static final int POINT_SIZE = 3;
+    /**
+     * Последняя СК окна
+     */
+    private CoordinateSystem2i lastWindowCS;
+
+    /**
+     * Задача
+     *
+     * @param ownCS  СК задачи
+     * @param points массив точек
+     */
+    public Task(CoordinateSystem2d ownCS, ArrayList<Point> points) {
+        this.ownCS = ownCS;
+        this.points = points;
+    }
+
+    /**
+     * Рисование задачи
+     *
+     * @param canvas   область рисования
+     * @param windowCS СК окна
+     */
+    public void paint(Canvas canvas, CoordinateSystem2i windowCS) {
+        // Сохраняем последнюю СК
+        lastWindowCS = windowCS;
+
+        canvas.save();
+        // создаём перо
+        try (var paint = new Paint()) {
+            for (Point p : points) {
+                // получаем цвет точки
+                paint.setColor(p.getColor());
+                // y-координату разворачиваем, потому что у СК окна ось y направлена вниз,
+                // а в классическом представлении - вверх
+                Vector2i windowPos = windowCS.getCoords(p.pos.x, p.pos.y, ownCS);
+                // рисуем точку
+                canvas.drawRect(Rect.makeXYWH(windowPos.x - POINT_SIZE, windowPos.y - POINT_SIZE, POINT_SIZE * 2, POINT_SIZE * 2), paint);
+            }
+        }
+        canvas.restore();
+    }
+
+    /**
+     * Добавить точку
+     *
+     * @param pos      положение
+     * @param pointSet множество
+     */
+    public void addPoint(Vector2d pos, Point.PointSet pointSet) {
+        Point newPoint = new Point(pos, pointSet);
+        points.add(newPoint);
+        PanelLog.info("точка " + newPoint + " добавлена в " + newPoint.getSetName());
+    }
+
+
+    /**
+     * Клик мыши по пространству задачи
+     *
+     * @param pos         положение мыши
+     * @param mouseButton кнопка мыши
+     */
+    public void click(Vector2i pos, MouseButton mouseButton) {
+        if (lastWindowCS == null) return;
+        // получаем положение на экране
+        Vector2d taskPos = ownCS.getCoords(pos, lastWindowCS);
+        // если левая кнопка мыши, добавляем в первое множество
+        if (mouseButton.equals(MouseButton.PRIMARY)) {
+            addPoint(taskPos, Point.PointSet.FIRST_SET);
+            // если правая, то во второе
+        } else if (mouseButton.equals(MouseButton.SECONDARY)) {
+            addPoint(taskPos, Point.PointSet.SECOND_SET);
+        }
+    }
+
+
+}
